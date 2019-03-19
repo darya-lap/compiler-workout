81e8a95de54a4c70150da56e466d2d5e623de97b
daria.laptsevich <daria.laptsevich@team.wrike.com>
2019-03-20 00:45:27 +0300

    hw4

--- src/Language.ml
+++ src/Language.ml
@@ -44,7 +44,39 @@ module Expr =
        Takes a state and an expression, and returns the value of the expression in 
        the given state.
      *)                                                       
-    let eval _ _ = failwith "Not yet implemented"
+     let bool_to_int b = if b then 1 else 0 
+     let int_to_bool i = i != 0 
+ 
+     let operator s =
+         match s with
+         | "+"  -> ( + )
+         | "-"  -> ( - )
+         | "*"  -> ( * )
+         | "/"  -> ( / )
+         | "%"  -> ( mod )
+         | "<"  -> fun left right -> bool_to_int (( < ) left right)
+         | ">"  -> fun left right -> bool_to_int (( > ) left right)
+         | "<=" -> fun left right -> bool_to_int (( <= ) left right)
+         | ">=" -> fun left right -> bool_to_int (( >= ) left right)
+         | "==" -> fun left right -> bool_to_int (( == ) left right)
+         | "!=" -> fun left right -> bool_to_int (( != ) left right)
+         | "&&" -> fun left right -> bool_to_int (( && ) (int_to_bool left) (int_to_bool right))
+         | "!!" -> fun left right -> bool_to_int (( || ) (int_to_bool left) (int_to_bool right))
+ 
+     let rec eval st exp =
+         match exp with
+         | Const value -> value
+         | Var name   -> st name
+         | Binop (op, left, right) -> (operator op) (eval st left) (eval st right);;
+ 
+     (* Expression parser. You can use the following terminals:
+ 
+          IDENT   --- a non-empty identifier a-zA-Z[a-zA-Z0-9_]* as a string
+          DECIMAL --- a decimal constant [0-9]+ as a string
+    
+     *)
+ 
+     let parseBinOp op = ostap(- $(op)), (fun x y -> Binop (op, x, y))
 
     (* Expression parser. You can use the following terminals:
 
@@ -52,8 +84,23 @@ module Expr =
          DECIMAL --- a decimal constant [0-9]+ as a string
                                                                                                                   
     *)
-    ostap (                                      
-      parse: empty {failwith "Not yet implemented"}
+    ostap (
+      expr:
+          !(Ostap.Util.expr
+            (fun x -> x)  (* identity function *)
+            (Array.map (fun (asc, ops) -> asc, List.map parseBinOp ops)
+                  [|
+                      `Lefta, ["!!"];
+                      `Lefta, ["&&"];
+                      `Nona , ["=="; "!="];
+                      `Nona , ["<="; "<"; ">="; ">"];
+                      `Lefta, ["+"; "-"];
+                      `Lefta, ["*"; "/"; "%"];
+                  |]
+              )
+            primary
+        );
+      primary: x:IDENT {Var x} | c:DECIMAL {Const c} | -"(" expr -")"  (* simpiest expression - {var, const, (var), (const)}*)
     )
     
   end
@@ -78,11 +125,27 @@ module Stmt =
 
        Takes a configuration and a statement, and returns another configuration
     *)
-    let eval _ _ = failwith "Not yet implemented"
+    let rec eval conf op =
+      let (st, input, output) = conf in
+      match op with
+        | Read var	-> (match input with
+                | x::rest -> (Expr.update var x st), rest, output
+                | [] -> failwith("No more input")
+              )
+        | Write exp      -> st, input, (output @ [Expr.eval st exp])
+        | Seq (first, second)       -> eval (eval conf first) second
+        | Assign (var, exp) -> (Expr.update var (Expr.eval st exp) st), input, output  
 
     (* Statement parser *)
     ostap (
-      parse: empty {failwith "Not yet implemented"}
+      unitStmt:
+  			  "read" "(" x:IDENT ")" {Read x}
+  			| "write" "(" e:!(Expr.expr) ")" {Write e}
+        | x:IDENT ":=" e:!(Expr.expr) {Assign(x, e)};
+
+      parse:
+          s:unitStmt ";" rest:parse {Seq(s, rest)}
+        | unitStmt
     )
       
   end
